6023c7ca87ed7a4fa92ee30b848a37ce5f39ce9b
Tetsuya <namajikona@gmail.com>
2017-08-17 02:10:36 +0900

    divide function

--- tmp/glsl.cpp
+++ tmp/glsl.cpp
@@ -6,18 +6,12 @@
 #include "glsl.h"
 
 Glsl::Glsl(){
-	GLenum glewStatus = glewInit();
-	if (glewStatus != GLEW_OK)
-	{
-		std::cout << "Error: " << glewGetErrorString(glewStatus) << std::endl;
-		std::exit(1);
-	}
+	init();
 
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	static const GLfloat g_vertex_buffer_data[] = {
+	GLuint vertexarrayID;
+	
+	GLuint array[2];
+	static GLfloat g_vertex_buffer_data[] = {
 		//1
 		-0.5f, 0.5f, 0.0f,
 		-0.5f, 0.4f, 0.0f,
@@ -40,14 +34,49 @@ Glsl::Glsl(){
 		0,1,1,1,1,1,0
 	};
 
-
+	//Buffer‚ÉŠÖ‚·‚éˆ—
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	buffer_process(vertexbuffer, *g_vertex_buffer_data, 39, 0);
+	
+	//”¯‚Ì’Tõ
+	std::vector<int> root_tip_index;
+	root_tip_index.push_back(0);
+	for (int i = 1; i < 13; i++){
+		if (index[i] == 0){
+			root_tip_index.push_back(i);
+		}
+	}
 
+	//•`‰æ‚ÉŠÖ‚·‚éˆ—
+	for (int i = 0; i < root_tip_index.size(); i = i + 2){
+		draw_line_strip(10, root_tip_index[i], root_tip_index[i + 1] + 1 - root_tip_index[i]);
+	}
+
+
+
+}
+
+Glsl::~Glsl(){
+
+}
+
+void Glsl::init(){
+	GLenum glewStatus = glewInit();
+	if (glewStatus != GLEW_OK)
+	{
+		std::cout << "Error: " << glewGetErrorString(glewStatus) << std::endl;
+		std::exit(1);
+	}
+}
+
+void Glsl::buffer_process(GLuint &buff , GLfloat &data ,int size,int index){
+	glEnableVertexAttribArray(index);
+	glBindBuffer(GL_ARRAY_BUFFER, buff);
+	glBufferData(GL_ARRAY_BUFFER, size * sizeof(GLfloat), &data, GL_DYNAMIC_DRAW);
+}
+
+void Glsl::draw_line_strip(int width, int begin_index, int num){
 	glVertexAttribPointer(
 		0,
 		3,
@@ -56,24 +85,11 @@ Glsl::Glsl(){
 		0,
 		(void*)0
 	);
-	
-	std::vector<int> root_tip_index;
-	root_tip_index.push_back(0);
-	for (int i = 1; i < 13; i++){
-		if (index[i] == 0){
-			root_tip_index.push_back(i);
-		}
-	}
-	for (int i = 0; i < root_tip_index.size(); i = i + 2){
-		//std::cout << "line  : " << root_tip_index[i] << " : " << root_tip_index[i  + 1] + 1  << std::endl;
-		glDrawArrays(GL_LINE_STRIP, root_tip_index[i], root_tip_index[i + 1] + 1  - root_tip_index[i]);
-	}
 
-
-	glDisableVertexAttribArray(0);
-
+	glLineWidth(width);
+	glDrawArrays(GL_LINE_STRIP, begin_index, num);	
 }
 
-Glsl::~Glsl(){
+void Glsl::draw_mesh(){
 
 }
--- tmp/glsl.h
+++ tmp/glsl.h
@@ -2,5 +2,8 @@ class Glsl{
 public:
 	Glsl();
 	~Glsl();
-
+	void init();
+	void buffer_process(GLuint &buff, GLfloat &data ,int size ,int index);
+	void draw_line_strip(int width, int begin_index, int num);
+	void draw_mesh();
 };
